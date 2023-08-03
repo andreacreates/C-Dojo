74198a5aaf157dc50872d112617368296bba03fd
andreacreates <andreaeu02@gmail.com>
2023-08-02 21:11:21 -0700

    Simple program with get_value() fucntion

---
+++ FirstCppProgram/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+const int get_value(){
+    return 3;
+}
+
+int main(){
+    
+    cout << "Hello World! " << get_value() << endl; 
+
+    return 0;
+}
