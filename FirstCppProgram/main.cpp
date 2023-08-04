f7625c7f7294af7620d78ff24903f17f3010609b
andreacreates <andreaeu02@gmail.com>
2023-08-04 13:17:01 -0700

    Adding Comments

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 
 using namespace std;
+/*
+    This program prints Hello World!
+    and has a function that returns a constant value.
+*/
 
+//Function get_value() return a constant value.
 const int get_value(){
     return 3;
 }
 
+//Main function
 int main(){
     
+    //Print Hello World! and print contant value in function get_value()
     cout << "Hello World! " << get_value() << endl; 
 
     return 0;
