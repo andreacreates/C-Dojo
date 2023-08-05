27bce479d7629e68838709552506c99d040b75a7
andreacreates <andreaeu02@gmail.com>
2023-08-05 13:00:15 -0700

    Add two numbers program using a function

---
+++ FirstCppProgram/addNumbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    This program has a function that add two numbers.
+*/
+
+//This fucntion takes two int and return the sum of both int parameters
+int addNumbers(int firstNumber, int secondNumber){
+    int sum = 0;
+    sum = firstNumber + secondNumber;
+    return sum;
+}
+
+int main(){
+
+    int first_number = 3;
+    int second_number = 77;
+
+    cout << "First Number : " << first_number << endl;
+    cout << "Second Number : " << second_number << endl;
+    cout << "The Sum is : " << addNumbers(first_number, second_number) << endl << endl; //Using addNumbers() passing two declared ints
+
+    cout << "The Sum of these two numbers 88 and 66 is " << addNumbers(88,66) << endl; // Using addNumbers() passing two numbers
+    
+    return 0;
+
+}
