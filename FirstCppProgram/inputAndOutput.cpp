355fabc03ca5b66cf499f03ac0d9a3b5800c6540
andreacreates <andreaeu02@gmail.com>
2023-08-21 18:29:00 -0700

    Fixed issue between cin and cin.getline by using cin.ignore()

--- FirstCppProgram/inputAndOutput.cpp
+++ FirstCppProgram/inputAndOutput.cpp
@@ -5,26 +5,25 @@ using namespace std;
 
 /*
     This program has diffrent examples of input and output
-    Part 1 has cout, cerr, clog, cin
+    Part 1 has cout, cerr, clog, cin, cin.ignore()
     Part 2 has cout and use getline(cin, variable) for input
-    Note: Part 2 won't work correctly if Part 1 is not commented
 */
 
 int main(){
-/*
+
     int age = 21;
     string name;
 
     //Printing data
-    cout << "Part one of the program just using output" << endl;
+    cout << "Part 1 of the program just using different type of data outputs and inputs." << endl;
     cout <<"The number is : " << 12 << endl;
     cout << "The age is : " << age << endl;
 
     //Error
-    cerr << "cerr output : Something went wrong" << endl;
+    cerr << "cerr output : Something went wrong." << endl;
 
     //Log message
-    clog << "clog output : This is a log message" << endl;
+    clog << "clog output : This is a log message." << endl;
 
     cout << "\nPlease type in your name : " << endl;
     cin >> name; //Input name
@@ -32,24 +31,25 @@ int main(){
     cout << "Please type in your age : " << endl;
     cin >> age; //Input age
 
-    cout << "Hello " << name << "! You are " << age << " years old" << endl;
+    cout << "Hello " << name << "! You are " << age << " years old." << endl;
 
     //Chaining cin
-    cout << "\nNow lets get your name nad age in on line using chaning cin :)" << endl;
+    cout << "\nNow lets get your name and age in on line using chaining cin." << endl;
     cout << "Please type in your last name and age, separated by spaces : " << endl;
     cin >> name >> age; //Input name and age
-    cout << "Hello " << name << "! You are " << age << " years old" << endl;
-*/
+    cin.ignore();
+    cout << "Hello " << name << "! You are " << age << " years old." << endl;
+
     int age1;
     string full_name;
     
     //Using getline() for data with space
-    cout << "\nPart 2 lets get your full name, we are using getline to get input with spaces" << endl;
+    cout << "\nPart 2 lets get your full name, we are using getline to get input with spaces." << endl;
     cout << "Please type in your full name : " << endl;
     getline(cin, full_name); //Input full name
     cout << "Please type in your age : " << endl;
     cin >> age1; //Input age
-    cout << "Hello " << full_name << "! You are " << age1 << " years old" << endl;
+    cout << "Hello " << full_name << "! You are " << age1 << " years old." << endl;
 
     return 0;
 }
