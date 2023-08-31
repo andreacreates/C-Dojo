760aab02f8749663b93e9e54a5cab538b6118c97
andreacreates <andreaeu02@gmail.com>
2023-08-30 18:04:24 -0700

    Correct interest earned now is making the correct calculation

--- OperationsOnData/interestEarned.cpp
+++ OperationsOnData/interestEarned.cpp
@@ -5,24 +5,29 @@ using namespace std;
 
 int main() {
 
-    //double interest_rate, time_compounded, principal, amount_in_savings;
+    // Variables
+    double interest_rate, 
+           principal, 
+           amount_in_savings;
+    int time_compounded; 
 
-    double interest_rate = 4.25;
-    int time_compounded = 12;
-    double principal = 1000; 
-    double amount_in_savings;
-    double result;
-/*
+    // Ask for the principal, interest rate and number of times compounded
     cout << "Enter balance in savings account : ";
     cin >> principal;
     cout << "Enter interest rate : ";
     cin >> interest_rate;
     cout << "How many times the interest is compounded during the year? : ";
     cin >> time_compounded;
-*/
-    result = pow((1 + (interest_rate/time_compounded)), time_compounded);
-    amount_in_savings = principal * result;
 
+    // Calculate
+    interest_rate /= 100 ;
+
+    // Amount = Principal * ((1 + Rate/T)^T)
+    amount_in_savings = principal *  pow((1 + (interest_rate/time_compounded)), time_compounded);
+
+    interest_rate *= 100 ;
+
+    // Display
     cout << setw(20) << left << "Interest Rate: ";
     cout << setw(12) << setprecision(2) << fixed << right << interest_rate << "%" << endl;
     cout << setw(20) << left << "Time Compounded: ";
